ede2bb8f1377b103445fb9ac674a26a8ce3b8ff2
Neil Huyton <github@nehu.me>
2026-03-28 12:43:09 +0000

    fix: env var

--- server/context.ts
+++ server/context.ts
@@ -1,7 +1,9 @@
+// server/context.ts
 import { PrismaClient } from "@prisma/client";
 import {
   extractToken,
   verifyTokenAndGetUserId,
+  type SupabaseConfig,
 } from "@steel-cut/trpc-shared/server";
 
 const globalForPrisma = globalThis as typeof globalThis & {
@@ -9,6 +11,10 @@ const globalForPrisma = globalThis as typeof globalThis & {
 };
 
 export const prisma = (globalForPrisma.prisma ??= new PrismaClient());
+
+const supabaseConfig: SupabaseConfig = {
+  supabaseUrl: process.env.SUPABASE_URL || "",
+};
 
 export interface Context {
   prisma: PrismaClient;
@@ -21,6 +27,6 @@ export async function createContext({
   req: Request;
 }): Promise<Context> {
   const token = extractToken(req);
-  const userId = await verifyTokenAndGetUserId(token);
+  const userId = await verifyTokenAndGetUserId(token, supabaseConfig);
   return { prisma, userId };
 }
