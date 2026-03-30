75d4cdb36284007328c339a7f3aab69e2fafa8ae
Neil Huyton <github@nehu.me>
2026-03-30 23:04:33 +0100

    fix: broken tests

--- src/routes/_authenticated/home.tsx
+++ src/routes/_authenticated/home.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated/home")({
   component: HomePage,
@@ -6,8 +6,13 @@ export const Route = createFileRoute("/_authenticated/home")({
 
 function HomePage() {
   return (
-      <h1>
-        Home
-      </h1>
+    <h1>
+      <Link
+        to="/stacks/$stackId"
+        params={{ stackId: "7b1ee88b-0202-4eb4-a321-df3c5a58ff18" }}
+      >
+        Link to Stack
+      </Link>
+    </h1>
   );
 }
